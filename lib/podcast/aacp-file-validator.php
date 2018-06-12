2840691dc7f369b51fcb77b4c2a4c23e0e0f417d
Chris Doernen <christian.doernen@icloud.com>
2018-06-12 09:51:16 +0200

    Add validate method

--- lib/podcast/aacp-file-validator.php
+++ lib/podcast/aacp-file-validator.php
@@ -1,9 +1,30 @@
 <?php
 
 class aacp_FileValidator {
+
+    public function validateAndSendEmail() {
+        $incorrectFiles = getIncorrectFiles();
+        
+        if(!empty($incorrectFiles)) {
+            
+        }
+    }
  
- 
-   
+    private function getIncorrectFiles() {
+        $sermonDirectory = get_home_path().'/../live_podcast/*/*.mp3';
+        $sermons = [];
+        
+        foreach(glob($sermonDirectory) as $file) {
+        	
+        	$regularExpression = "/\d{4}-\d{2}-\d{2}_.+_.+\.mp3/";
+        	if(!preg_match($regularExpression, $file))
+        	{
+                array_push($sermons, $sermon);
+        	}
+        }
+        
+        return(rsort($sermons));
+    }
 }
 
 ?>
