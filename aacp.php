056cb50cedd2570d2a4e302ab9e09226f1f6742c
Chris Doernen <christian.doernen@icloud.com>
2018-06-20 23:52:52 +0200

    Update changelog and version

--- aacp.php
+++ aacp.php
@@ -9,7 +9,7 @@
  * Plugin Name:       ARCHE Augsburg Communication Plugin
  * Plugin URI:        https://arche-augsburg.de
  * Description:       For communication issues
- * Version:           1.0.2
+ * Version:           1.0.3
  * Author:            Christian Doernen, Michael Machus
  * Author URI:        
  * License:           
@@ -30,7 +30,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'PLUGIN_NAME_VERSION', '1.0.2' );
+define( 'PLUGIN_NAME_VERSION', '1.0.3' );
 
 /**
  * The code that runs during plugin activation.
