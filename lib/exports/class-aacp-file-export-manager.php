a999e067802ce041d50e64d7b99dc7c29841cd5e
Chris Doernen <christian.doernen@icloud.com>
2018-06-22 12:42:38 +0200

    Further improve fetching of events for export

--- lib/exports/class-aacp-file-export-manager.php
+++ lib/exports/class-aacp-file-export-manager.php
@@ -22,16 +22,29 @@ class aacp_FileExportManager {
     }
     
 	private function export_newsletter ( $month ) {
-		 $events_to_print = $this->get_events( $month );
+		 $events_to_print = $this->query_events( $month );
 		 $file_renderer = new aacp_FileRenderer();
 		 return $file_renderer->render_newsletter_and_get_url( $events_to_print );
 	}
 	
-	private function get_events ( $month ) {
+	private function query_events ( $month ) {
 		$events = array();
+		$start_date = date( 'Y' ) . '-' . $month . '-1';
 		
 		$argu = array(
             'post_type' => 'events',
+            'orderby' => 'meta_value_num', 
+            'meta_key'=> 'aa_event_start_datetime',
+            'order' => 'ASC',
+            'meta_value' => strtotime( $start_date ),
+            'meta_compare' => '>',
+			'tax_query' => array(
+				array(
+			        'taxonomy' => 'Anzeigeeinstellungen',
+			        'field' => 'slug',
+			        'terms' => 'arche-termine-print',
+			    ),
+			)
         );
 
 		$query = new WP_Query($argu);
@@ -40,22 +53,69 @@ class aacp_FileExportManager {
     		while ($query->have_posts()) 
     		{
 	            $query->the_post();
-	            
-	            $uploads = wp_upload_dir();
-	            
-				$event = array();
-	            $event['permalink'] = get_the_permalink();
-	            $event['post_title'] = get_the_title();
-	            $image_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'medium' )[0];
-	            $event['image_url'] = $image_url;
-	            $event['image_path'] = str_replace( $uploads['baseurl'], $uploads['basedir'], $image_url );
-	            $event['content'] = get_the_content();
+	        	$event = $this->get_event();
 	            array_push($events, $event);
     		}
     	}
 		wp_reset_postdata();
 		
 		return $events;
+	}
+	
+	private function get_event() {
+		$event = array();
+		
+        $event['permalink'] = get_the_permalink();
+        $event['post_title'] = get_the_title();
+        $event['image'] = $this->get_event_image();
+        $event['content'] = get_the_content();
+        $event['date'] = $this->get_datetime_string();
+        
+        return $event;
+	}
+	
+	private function get_event_image() {
+		$image = array();
+		
+		if ( rwmb_meta( 'aa_event_gx' , null, $post->ID  ) != "") {
+			$args = array('size' => "large", 'type' => 'image');
+			$images = rwmb_meta( 'aa_event_gx', $args, $post->ID  );
+			
+			if ( count($images) > 0 ) {
+				$image['path'] = reset($images)['path'];
+				$image['url'] = reset($images)['url'];
+			}
+		}
+		
+		return $image;
+	}
+	
+	private function get_datetime_string() {
+		$start = rwmb_meta('aa_event_start_datetime');
+		$end = rwmb_meta('aa_event_end_datetime');
+        
+        // Start and end date are the same
+		if (empty($end) || date('Ymd', $start) == date('Ymd', $end)) 
+		{
+            $datetime = strftime('%a, %d. %b', $start);
+            $end = $start;
+        }
+        // Start and end date are different
+        else 
+        {
+            // Same month
+            if(date('Ym', $start) == date('Ym', $end))
+            {
+                $datetime = strftime('%a, %d.', $start).' - '.strftime('%a, %d. %b', $end);
+            }
+            // Different month
+            else
+            {
+                $datetime = strftime('%a, %d. %b', $start).' - '.strftime('%a, %d. %b', $end);
+            }
+        }
+        
+        return $datetime;
 	}
 	
 	public function get_month_of_export_newsletter() {
@@ -68,6 +128,8 @@ class aacp_FileExportManager {
 		
 		$export_month = array();
 		
+		// If we are after the mid of a month, export the newsletter
+		// for the next month
 		if ( $actual_day > 15 ) {
 		    $export_month_number = $actual_month + 1;
 		}
