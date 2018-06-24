722cd591fe46b5bd4e740d5607262bdf55b933f0
Chris Doernen <christian.doernen@icloud.com>
2018-06-24 08:20:41 +0200

    Add job for presentation export

--- includes/class-aacp-cronjobmanager.php
+++ includes/class-aacp-cronjobmanager.php
@@ -6,6 +6,10 @@ class aacp_CronJobManger {
     	$this->schedule_cron_job( 'weekly', 'podcast_file_validation_job' );
     }
     
+    public function schedule_cron_job_presentation_export() {
+    	$this->schedule_cron_job( 'daily', 'presentation_export_job' );
+    }
+    
 	private function schedule_cron_job($intervall, $jobName){
 	    if( !wp_next_scheduled( $jobName ) ) {  
     	   wp_schedule_event( time(), $intervall, $jobName );  
@@ -14,6 +18,10 @@ class aacp_CronJobManger {
 	
 	public function unschedule_cron_job_podcast_file_validation(){
 		$this->unschedule_cron_job('podcast_file_validation_job');
+	}
+	
+	public function unschedule_cron_job_presentation_export() {
+		$this->unschedule_cron_job('presentation_export_job');
 	}
 	
     private function unschedule_cron_job($jobName) {
