b3a34cb93d3c2458ae21011673affa8ab98709ac
Chris Doernen <christian.doernen@icloud.com>
2018-06-24 08:41:30 +0200

    Add class owncloud adapter

---
+++ lib/exports/class-aacp-owncloud-adapter.php
@@ -0,0 +1,19 @@
+<?php
+
+class aacp_OwncloudAdapter {
+    
+    private $username;
+    private $password;
+    private $upload_directory;
+    
+    public upload_service_presentation_export () {
+        
+    }
+    
+    private function upload_file( $file_name, $directory ) {
+        
+    }
+    
+}
+
+?>
