fdbb98aa837f2b3f34c7860050d8115495a743ad
Chris Doernen <christian.doernen@icloud.com>
2018-07-12 18:49:33 +0200

    Refactoring of constants used multiple time

--- lib/exports/class-aacp-file-renderer.php
+++ lib/exports/class-aacp-file-renderer.php
@@ -1,6 +1,16 @@
 <?php
 
 class aacp_FileRenderer {
+	
+	private $defaultTextColor;
+	private $dateTimeSpanColor;
+	private $defaultFont;
+
+	public function __construct (){
+		$this->defaultTextColor = "7d7d7d";
+		$this->dateTimeSpanColor = "555555";
+		$this->defaultFont = "'Helvetica Neue";
+	}
 
 	public function render_newsletter( $events_to_print, $file_name ) {
 		
@@ -10,25 +20,57 @@ class aacp_FileRenderer {
 		$text = "Januar 2018";
 
 		$boldFontStyleName = 'BoldText';
-		$phpWord->addFontStyle($boldFontStyleName, array('bold' => true, 'name' => 'Helvetica Neue', 'color' => 'darkGray', 'size' => 15));
+		$phpWord->addFontStyle($boldFontStyleName, 
+			array(
+				'bold' => true, 
+				'name' => $this->defaultFont, 
+				'color' => 'darkGray', 
+				'size' => 15
+			)
+		);
 
 		$terminDateFontStyleName = 'terminDate';
-		$phpWord->addFontStyle($terminDateFontStyleName, array('name' => 'Helvetica Neue', 'bgColor' => 'darkGray', 'color' => 'white', 'size' => 11));
+		$phpWord->addFontStyle($terminDateFontStyleName, 
+			array(
+				'name' => $this->defaultFont, 
+				'bgColor' => $this->dateTimeSpanColor, 
+				'color' => 'white', 
+				'size' => 11,
+			)
+		);
 
 		$terminHeaderFontStyleName = 'terminHeader';
-		$phpWord->addFontStyle($terminHeaderFontStyleName, array('bold' => true, 'name' => 'Helvetica Neue', 'color' => 'darkGray', 'size' => 11));
+		$phpWord->addFontStyle($terminHeaderFontStyleName, 
+			array(
+				'bold' => true, 
+				'name' => $this->defaultFont, 
+				'color' => $this->defaultTextColor, 
+				'size' => 13
+			)
+		);
 
 		$terminTextFontStyleName = 'terminText';
-		$phpWord->addFontStyle($terminTextFontStyleName, array('name' => 'Helvetica Neue', 'color' => 'darkGray', 'size' => 10));
+		$phpWord->addFontStyle($terminTextFontStyleName, 
+			array(
+				'name' => $this->defaultFont, 
+				'color' => $this->defaultTextColor, 
+				'size' => 10
+			)
+		);
 
 		$rightParagraph = 'RightParagraph';
-		$phpWord->addParagraphStyle($rightParagraph, array('align' => 'right', 'spaceAfter' => 1000));
+		$phpWord->addParagraphStyle($rightParagraph, 
+			array(
+				'align' => 'right', 
+				'spaceAfter' => 1000
+			)
+		);
 
 		$terminTextParagraph = 'terminTextParagraph';
 		$phpWord->addParagraphStyle($terminTextParagraph, array('align'=>'both', 'spaceAfter'=>50));
 		// New portrait section
 		$section = $phpWord->addSection(
-			array('marginLeft' => \PhpOffice\PhpWord\Shared\Converter::cmToTwip(1),
+			array('marginLeft' => \PhpOffice\PhpWord\Shared\Converter::cmToTwip(1.5),
 				'marginRight' => \PhpOffice\PhpWord\Shared\Converter::cmToTwip(1.5),
 				'marginTop' => \PhpOffice\PhpWord\Shared\Converter::cmToTwip(1.5),
 				'marginBottom' => \PhpOffice\PhpWord\Shared\Converter::cmToTwip(1.5))
@@ -40,7 +82,7 @@ class aacp_FileRenderer {
 
 		foreach($events_to_print as $event) {
 			$table->addRow();
-			$table->addCell(\PhpOffice\PhpWord\Shared\Converter::cmToTwip(5))->addImage($event['image']['path'], array(
+			$table->addCell(\PhpOffice\PhpWord\Shared\Converter::cmToTwip(7))->addImage($event['image']['path'], array(
 				'width'            => \PhpOffice\PhpWord\Shared\Converter::cmToPixel(5.5),
 				'posHorizontal'    => \PhpOffice\PhpWord\Style\Image::POSITION_HORIZONTAL_CENTER,
 				'alignment'        => \PhpOffice\PhpWord\SimpleType\Jc::CENTER
@@ -53,9 +95,8 @@ class aacp_FileRenderer {
 
 		// Saving the document as OOXML file...
 		$objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
-		$objWriter->save($file_name); //__DIR__ . '/../../../../uploads/aacp-exports/CI-ARCHE.docx');		
+		$objWriter->save($file_name);		
 	}
-	
 }
 
 ?>
